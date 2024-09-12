12fc18a266ae31a03d28daa1c291383db0b2764d
unknown <abdurashidovmirsaid82@gmail.com>
2024-09-12 17:01:23 +0500

    project

--- app.mjs
+++ app.mjs
@@ -1,32 +1,58 @@
 import express from "express"
-import bodyParser from "body-parser";
-import fantasy from "./route/fantasyWarmovie.mjs";
-import kartoon from "./route/kartoon.mjs";
-import aanimal from "./route/aanimal.mjs";
-import cconsols from "./route/cconsols.mjs";
-import ccosmos from "./route/ccosmos.mjs";
-import ddinosaur from "./route/ddinosaur.mjs";
-import gguns from "./route/gguns.mjs";
-import mmebels from "./route/mmebels.mjs";
-import plene from "./route/plene.mjs";
-import tehnike from "./route/tehnike.mjs";
+
+import animal from "./db/animal.mjs";
+import cartoon from "./db/cartoon.mjs";
+import consols from "./db/consols.mjs";
+import cosmos from "./db/cosmos.mjs";
+import dinosaur from "./db/dinosaur.mjs"
+import guns from "./db/guns.mjs";
+import mebel from "./db/mebel.mjs";
+import plane from "./db/plane.mjs";
+import tehnika from "./db/tehnika.mjs";
+import warmovie from "./db/warmovie.mjs";
+
+
+
+
 
 const port = 3000;
 
 const app = express()
 
-app.use(fantasy);
-app.use(kartoon);
-app.use(aanimal);
-app.use(cconsols);
-app.use(ccosmos);
-app.use(ddinosaur);
-app.use(gguns);
-app.use(mmebels);
-app.use(plene);
-app.use(tehnike);
+app.get("/cartoon", (req,res) =>{
+   res.json(cartoon)
+})
+app.get("/consols", (req,res) =>{
+   res.json(consols)
+})
+app.get("/cosmos", (req,res) =>{
+   res.json(cosmos)
+})
+app.get("/guns", (req,res) =>{
+   res.json(guns)
+})
+app.get("/dinosaur", (req,res) =>{
+   res.json(dinosaur)
+})
+app.get("/mebel", (req,res) =>{
+   res.json(mebel)
+})
+app.get("/plane", (req,res) =>{
+   res.json(plane)
+})
+app.get("/tehnika", (req,res) =>{
+   res.json(tehnika)
+})
+app.get("/warmovie", (req,res) =>{
+   res.json(warmovie)
+})
 
-app.use(bodyParser.json())
+
+app.get("/animal", (req,res) =>{
+    res.json(animal);
+});
+
+
 
 
 
